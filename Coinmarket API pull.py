fa203ed7bdaabcb817388660b3cf72f674dabb7d
Ollie S <ovcsergeant@gmail.com>
2021-11-22 18:57:59 +1300

    V.10 of bot

---
+++ Coinmarket API pull.py
@@ -0,0 +1,25 @@
+
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'f7e49c8e-7e10-472f-afe9-78a73ddc12ab',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
