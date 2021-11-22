fec2d68352d3aa3b149f08b1b08e8dbff29e8f76
Ollie S <ovcsergeant@gmail.com>
2021-11-22 20:44:21 +1300

    Added function (make_request) to be more generic. Split URL into endpoints plus base url

--- Coinmarket API pull.py
+++ Coinmarket API pull.py
@@ -7,6 +7,7 @@ baseurl = 'https://pro-api.coinmarketcap.com'
 endpoint_listing_latest = '/v1/cryptocurrency/listings/latest'
 endpoint_quotes_latest = '/v1/cryptocurrency/quotes/latest'
 
+
 parameters = {
   'start':'1',
   'limit':'5000',
@@ -26,15 +27,17 @@ session.headers.update(headers)
 
 def make_request(url,endpoint,parameters):
   try:
-    response = session.get(url + endpoint, parameters)
+    full = url + endpoint
+
+    response = session.get(full, params=parameters)
     data = json.loads(response.text)
-    #print(data)
+    print(data)
   except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
 
 
-make_request(baseurl,endpoint_quotes_latest,parameters2)
-
+make_request(baseurl, endpoint_quotes_latest, parameters2)
+#make_request(baseurl, endpoint_listing_latest, parameters)
 
 #print(data['data']['1']['quote']['USD']['price'])
 
