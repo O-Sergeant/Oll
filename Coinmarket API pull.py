d8b240893039c5b05f6f28e802a355aa5e27639c
Ollie S <ovcsergeant@gmail.com>
2021-11-22 20:36:46 +1300

    added virtual environment

--- Coinmarket API pull.py
+++ Coinmarket API pull.py
@@ -1,9 +1,12 @@
-
+import a as a
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+baseurl = 'https://pro-api.coinmarketcap.com'
+endpoint_listing_latest = '/v1/cryptocurrency/listings/latest'
+endpoint_quotes_latest = '/v1/cryptocurrency/quotes/latest'
+
 parameters = {
   'start':'1',
   'limit':'5000',
@@ -13,15 +16,25 @@ headers = {
   'Accepts': 'application/json',
   'X-CMC_PRO_API_KEY': 'f7e49c8e-7e10-472f-afe9-78a73ddc12ab',
 }
+parameters2 = {
+  'id':'1',
+
+}
 
 session = Session()
 session.headers.update(headers)
 
-try:
-  response = session.get(url, params=parameters)
-  data = json.loads(response.text)
-  #print(data)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+def make_request(url,endpoint,parameters):
+  try:
+    response = session.get(url + endpoint, parameters)
+    data = json.loads(response.text)
+    #print(data)
+  except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
 
-print(parameters.get('start'))
+
+make_request(baseurl,endpoint_quotes_latest,parameters2)
+
+
+#print(data['data']['1']['quote']['USD']['price'])
+
